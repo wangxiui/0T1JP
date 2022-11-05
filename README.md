bb1938bd4220c28db63434bb98668548e373d357
wangxiui <1036900394@qq.com>
2022-11-05 20:30:46 +0800

    feat: 官方入门教程（45%）

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # 0T1JP
-从0开始学到N1
+> 从0开始学到N1
+
+## 安装
+[入门教程](https://www.nextjs.cn/learn/basics/create-nextjs-app/setup)
+
+如果不成功，请查看 [此页面](https://github.com/vercel/next-learn/blob/master/basics/errors/install.md)。
+
+### 运行
+```
+pnpm run dev
+```
+
+### 知识点
+1. 文件目录+名称 = 路由url  
+2. [classname库](https://github.com/JedWatson/classnames)
+3. tailwind和classname库不冲突
+4. css Modules和sass一起用，文件名后缀：.module.sass
+5. 
---
+++ pages/posts/first-post.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Image from 'next/image';
+import Head from 'next/head';
+import Script from "next/script";
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }></Script>
+
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+
+      <Image
+        src="/images/profile.png" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+    </Layout>
+  )
+}
