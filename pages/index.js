5e49d7d531c29553b6bac0cae83682ad7d502646
wangxiui <1036900394@qq.com>
2022-11-06 17:50:03 +0800

    fix: modify index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import Date from '../components/date';
 
 
-export default function Home({allPostsData}) {
+export default function Home() {
   return (
     <Layout home>
       <Head>
@@ -23,34 +23,9 @@ export default function Home({allPostsData}) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
+          <li><h1>1111</h1></li>
         </ul>
       </section>
     </Layout>
   )
 }
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-/*export async function getServerSideProps(context) {
-  return {
-    props: {
-      // props for your component
-    },
-  };
-}*/
