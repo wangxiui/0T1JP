32df8eec06573043f8fd5769871a4f1428cb93ae
wangxiui <1036900394@qq.com>
2022-11-06 18:53:04 +0800

    fix: delete utils.module.css

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import utilStyles from '../styles/utils.module.css';
-
 export default function Home() {
   return (
     <>
-      <section className={utilStyles.headingMd}>
+      <section>
         <p>Hello, I'm wangxiui. Welcome!</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
@@ -11,9 +9,9 @@ export default function Home() {
         </p>
       </section>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
+      <section >
+        <h2>Blog</h2>
+        <ul>
           <li><h1>1111</h1></li>
         </ul>
       </section>
