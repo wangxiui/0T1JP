b27703d75cc4b3386123c9bf497449152b85e802
wangxiui <1036900394@qq.com>
2022-11-13 21:46:47 +0800

    fix: fix a ts any type error

--- lib/posts.ts
+++ lib/posts.ts
@@ -76,7 +76,7 @@ export function getAllPostIds() {
   });*/
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
